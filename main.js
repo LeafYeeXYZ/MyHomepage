ed1b17d4582597c44c6d101e342e7604c0fa2ff2
LeafYeeXYZ <leafyeexyz@outlook.com>
2023-12-13 12:56:03 +0800

    添加缓存功能

--- main.js
+++ main.js
@@ -82,3 +82,24 @@ function switchBackground() {
 }
 
 
+// 阻止选择文本
+document.addEventListener('selectstart', function(e) {
+    e.preventDefault();
+});
+
+// 注册service worker
+if ('serviceWorker' in navigator) {
+    // 浏览器支持 Service Workers
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+            // 注册成功
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(function(err) {
+            // 注册失败
+            console.log('ServiceWorker registration failed: ', err);
+        });
+} else {
+    // 浏览器不支持 Service Workers
+    console.log('service worker is not supported');
+}

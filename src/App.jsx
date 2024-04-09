4416568fc75310d7e0799e69693f94e6ee772b3a
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-09 14:03:12 +0800

    up

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,30 @@ function App() {
       setEssay(data.hitokoto)
     })
     // 天气
-    fetch(`https://devapi.qweather.com/v7/weather/3d?location=101010100&key=${import.meta.env.VITE_WEATHER_API_KEY}`)
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
-      const daily = data.daily[0]
-      const date = new Date()
-      const month = date.getMonth() + 1
-      const day = date.getDate()
-      const week = ['天', '一', '二', '三', '四', '五', '六'][date.getDay()]
-      setWeather(`${month}月${day}日, 星期${week}, 天气${daily.textDay}, ${daily.tempMin}-${daily.tempMax}℃; 明日${data.daily[1].textDay}, ${data.daily[1].tempMin}-${data.daily[1].tempMax}℃`)
-    })
-  })
+    const weatherExpire = localStorage.getItem('weatherExpire')
+    const weather = localStorage.getItem('weather')
+    if (weatherExpire && weather && Date.now() < weatherExpire) {
+      setWeather(weather)
+    } else {
+      fetch(`https://devapi.qweather.com/v7/weather/3d?location=101010100&key=${import.meta.env.VITE_WEATHER_API_KEY}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        const daily = data.daily[0]
+        const date = new Date()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        const week = ['天', '一', '二', '三', '四', '五', '六'][date.getDay()]
+        const text = `${month}月${day}日, 星期${week}, 天气${daily.textDay}, ${daily.tempMin}-${daily.tempMax}℃; 明日${data.daily[1].textDay}, ${data.daily[1].tempMin}-${data.daily[1].tempMax}℃`
+        setWeather(text)
+        // 存入 localStorage
+        localStorage.setItem('weather', text)
+        const weatherExpire = Date.now() + 60 * 60 * 1000
+        localStorage.setItem('weatherExpire', weatherExpire)
+      })
+    }
+  }, [])
   // 返回 JSX 元素
   return (
     <main className='container'>

276586a954ac1d5dc9f1240368ace1754b02345e
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-07-04 16:36:22 +0800

    根据地点显示天气

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -11,22 +11,26 @@ export function Weather() {
     if (weatherExpire && weather && Date.now() < weatherExpire) {
       setWeather(weather)
     } else {
-      fetch(`https://api.leafyee.xyz/weather`)
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        const daily = data.daily[0]
-        const date = new Date()
-        const month = date.getMonth() + 1
-        const day = date.getDate()
-        const week = ['天', '一', '二', '三', '四', '五', '六'][date.getDay()]
-        const text = `${month}月${day}日, 星期${week}, 天气${daily.textDay}, ${daily.tempMin}-${daily.tempMax}℃; 明日${data.daily[1].textDay}, ${data.daily[1].tempMin}-${data.daily[1].tempMax}℃`
-        setWeather(text)
-        // 存入 localStorage
-        localStorage.setItem('weather', text)
-        const weatherExpire = Date.now() + 60 * 60 * 1000
-        localStorage.setItem('weatherExpire', weatherExpire.toString())
+      // 获取位置
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        fetch(`https://api.leafyee.xyz/weather?location=${longitude.toFixed(2)},${latitude.toFixed(2)}`)
+        .then(res => {
+          return res.json()
+        })
+        .then(data => {
+          const daily = data.daily[0]
+          const date = new Date()
+          const month = date.getMonth() + 1
+          const day = date.getDate()
+          const week = ['天', '一', '二', '三', '四', '五', '六'][date.getDay()]
+          const text = `${month}月${day}日, 星期${week}, 天气${daily.textDay}, ${daily.tempMin}-${daily.tempMax}℃; 明日${data.daily[1].textDay}, ${data.daily[1].tempMin}-${data.daily[1].tempMax}℃`
+          setWeather(text)
+          // 存入 localStorage
+          localStorage.setItem('weather', text)
+          const weatherExpire = Date.now() + 60 * 60 * 1000
+          localStorage.setItem('weatherExpire', weatherExpire.toString())
+        })
       })
     }
   }, [])

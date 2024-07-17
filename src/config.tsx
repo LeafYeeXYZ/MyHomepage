54871fb6d410d8a034edf67682501687c81e5a98
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-07-17 10:09:40 +0800

    up

--- src/config.tsx
+++ src/config.tsx
@@ -9,6 +9,7 @@ import {
   StarFilled,
   SunFilled,
   BilibiliOutlined,
+  CloudOutlined,
 } from '@ant-design/icons'
 
 export type Link = {
@@ -21,8 +22,9 @@ export const links: Link[] = [
   { url: 'https://one.bnu.edu.cn/', name: '北京师范大学', icon: <BankFilled /> },
   { url: 'https://www.leafyee.xyz/', name: '小站', icon: <HomeFilled /> },
   { url: 'https://blog.leafyee.xyz/', name: '博客', icon: <BulbFilled /> },
-  { url: 'https://paint.leafyee.xyz/', name: '赛博画师小叶子', icon: <PictureFilled /> },
-  { url: 'https://comment.leafyee.xyz/ui', name: 'Waline', icon: <CalendarFilled /> },
+  { url: 'https://pan.leafyee.xyz/', name: '网盘', icon: <CloudOutlined /> },
+  { url: 'https://paint.leafyee.xyz/', name: '画画', icon: <PictureFilled /> },
+  { url: 'https://comment.leafyee.xyz/ui', name: '评论', icon: <CalendarFilled /> },
   { url: 'https://github.com', name: 'GitHub', icon: <GithubFilled /> },
   { url: 'https://dash.cloudflare.com', name: 'Cloudflare', icon: <CloudFilled /> },
   { url: 'https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx', name: 'NewBing', icon: <StarFilled /> },
